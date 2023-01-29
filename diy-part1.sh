04a73a387fe52a06ec89a304fe99a408fa960dae
fireh1974 <64630396+fireh1974@users.noreply.github.com>
2023-01-29 18:24:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,22 +15,12 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
-echo '添加jerrykuku的argon-mod主题'
-#rm -rf ./feeds/luci/themes/luci-theme-argon
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git ./feeds/luci/themes/luci-theme-argon
-echo '=========Add argon-mod OK!========='
 
 echo '添加alist'
-
 git clone https://github.com/sbwml/luci-app-alist package/alist
-
 echo '=========alist OK!========='
 
 
